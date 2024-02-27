d9f1cb017cb44007aba71d9a4e22c583c3d6488f
Anton P. <made1of1fire@gmail.com>
2024-02-27 13:23:54 +0300

    Start new spring app. Created body of app

---
+++ sring_app/src/main/java/main/entity/Note.java
@@ -0,0 +1,31 @@
+package main.entity;
+
+public class Note {
+    private int id;
+    private String header;
+    private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
