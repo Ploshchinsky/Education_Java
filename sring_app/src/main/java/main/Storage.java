5870c5a0443be8e562c0e610c14c1bd589180bde
Anton P. <made1of1fire@gmail.com>
2024-02-27 14:57:16 +0300

    Update Storage

--- sring_app/src/main/java/main/Storage.java
+++ sring_app/src/main/java/main/Storage.java
@@ -15,22 +15,8 @@ public class Storage {
         return id;
     }
 
-    public static int update(Note note) {
-        noteMap.replace(note.getId(), note);
-        return note.getId();
-    }
-
-    public static Note get(int id) {
-        return noteMap.getOrDefault(id, null);
-    }
-
     public static ArrayList<Note> list() {
         return noteMap.isEmpty() ? null : new ArrayList<>(noteMap.values());
-    }
-
-    public static int delete(int id) {
-        noteMap.remove(id);
-        return id;
     }
 
     public static boolean exist(int id) {
