0e46900ac4372f649db405e6e0dccbdbdf3451e8
Anton P. <made1of1fire@gmail.com>
2024-02-28 23:38:41 +0300

    Update Note -> Now it ORM object?

--- sring_app/src/main/java/main/model/Note.java
+++ sring_app/src/main/java/main/model/Note.java
@@ -1,8 +1,16 @@
 package main.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.lang.Integer;
 
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String text;
 
