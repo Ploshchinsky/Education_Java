49c7aa399587ee93f947706ee08d3b1000047cb5
Anton P. <made1of1fire@gmail.com>
2024-03-01 18:06:59 +0300

    fix: Minor update

--- sring_app/src/main/java/main/model/DefaultController.java
+++ sring_app/src/main/java/main/model/DefaultController.java
@@ -1,6 +1,7 @@
 package main.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,15 @@ import java.util.List;
 @Controller
 public class DefaultController {
     @Autowired
-    NoteRepository noteRepository;
+    private NoteRepository noteRepository;
+    @Value("${string.from.cfg}")
+    private String message;
 
     @RequestMapping("/")
     public String index(Model model) {
         List<Note> noteList = (List<Note>) noteRepository.findAll();
         model.addAttribute("notes", noteList);
+        model.addAttribute("message", message);
         return "index";
     }
 }
